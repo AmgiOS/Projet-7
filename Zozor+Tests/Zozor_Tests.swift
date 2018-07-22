e2d1fb73057342efd4a3daa2a2ba48cf39390505
hakimamg <hakimdu9347@gmail.com>
2018-07-22 18:58:12 +0200

    Projet7

--- Zozor+Tests/Zozor_Tests.swift
+++ Zozor+Tests/Zozor_Tests.swift
@@ -68,15 +68,4 @@ class Zozor_Tests: XCTestCase {
         
         XCTAssert(operations.calculateTotal() == "10")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
 }
